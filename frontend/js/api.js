50727e4ca577bfb9b9e8509304b3d48e1dd5c071
Carol Madella <carolina.madella@gmail.com>
2024-07-29 10:11:47 -0500

    update render url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://wdd330-sacrament-meeting-agenda.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
 
 // Fetch all meetings
 async function fetchMeetings() {
